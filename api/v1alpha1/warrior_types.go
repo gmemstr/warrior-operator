792eb53caa490709d5f617da98e0a8a58321a354
Gabriel Simmer <g@gmem.ca>
2025-04-14 17:44:06 +0100

    Initial commit of POC operator

---
+++ api/v1alpha1/warrior_types.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// WarriorSpec defines the desired state of Warrior
+type WarriorSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Project    string `json:"project"`
+	Downloader string `json:"downloader,omitempty"`
+	// +kubebuilder:default:={"maximum":1,"concurrency":5}
+	Scaling WarriorScaling `json:"scaling,omitempty"`
+}
+
+type WarriorScaling struct {
+	Minimum int `json:"minimum,omitempty"`
+	// +kubebuilder:default:=1
+	Maximum int `json:"maximum"`
+	// +kubebuilder:default:=5
+	Concurrency int `json:"concurrency"`
+}
+
+// WarriorStatus defines the observed state of Warrior
+type WarriorStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
+	PodCount   int                `json:"pod_count"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Warrior is the Schema for the warriors API
+// //+kubebuilder:subresource:status
+type Warrior struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   WarriorSpec   `json:"spec,omitempty"`
+	Status WarriorStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// WarriorList contains a list of Warrior
+type WarriorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Warrior `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Warrior{}, &WarriorList{})
+}
---
+++ internal/controller/warrior_controller.go
@@ -0,0 +1,302 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	warriorv1alpha1 "git.gmem.ca/arch/warrior-operator/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// WarriorReconciler reconciles a Warrior object
+type WarriorReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// Definitions to manage status conditions
+const (
+	// typeAvailableMemcached represents the status of the Deployment reconciliation
+	typeAvailableWarrior = "Available"
+	// typeDegradedMemcached represents the status used when the custom resource is deleted and the finalizer operations are yet to occur.
+	typeDegradedWarrior = "Degraded"
+)
+
+// +kubebuilder:rbac:groups=warrior.k8s.gmem.ca,resources=warriors,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=warrior.k8s.gmem.ca,resources=warriors/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=warrior.k8s.gmem.ca,resources=warriors/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+func (r *WarriorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	// TODO(user): your logic here
+	warrior := &warriorv1alpha1.Warrior{}
+	err := r.Get(ctx, req.NamespacedName, warrior)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	found := &appsv1.Deployment{}
+	err = r.Get(ctx, types.NamespacedName{Name: warrior.Name, Namespace: warrior.Namespace}, found)
+	if err != nil && apierrors.IsNotFound(err) {
+		// Define a new deployment
+		dep, err := r.deploymentForWarrior(warrior)
+		if err != nil {
+			log.Error(err, "Failed to define new Deployment resource for Warrior")
+
+			// The following implementation will update the status
+			meta.SetStatusCondition(&warrior.Status.Conditions, metav1.Condition{Type: typeAvailableWarrior,
+				Status: metav1.ConditionFalse, Reason: "Reconciling",
+				Message: fmt.Sprintf("Failed to create Deployment for the custom resource (%s): (%s)", warrior.Name, err)})
+
+			if err := r.Status().Update(ctx, warrior); err != nil {
+				log.Error(err, "Failed to update Warrior status")
+				return ctrl.Result{}, err
+			}
+
+			return ctrl.Result{}, err
+		}
+
+		log.Info("Creating a new Deployment",
+			"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		if err = r.Create(ctx, dep); err != nil {
+			log.Error(err, "Failed to create new Deployment",
+				"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+
+		// Deployment created successfully
+		// We will requeue the reconciliation so that we can ensure the state
+		// and move forward for the next operations
+		return ctrl.Result{RequeueAfter: time.Minute}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get Deployment")
+		return ctrl.Result{}, err
+	}
+	targetReplicas, err := replicasForWarrior(warrior)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if *found.Spec.Replicas != targetReplicas {
+		found.Spec.Replicas = &targetReplicas
+		if err = r.Update(ctx, found); err != nil {
+			log.Error(err, "Failed to update Deployment replicas")
+			return ctrl.Result{}, nil
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *WarriorReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&warriorv1alpha1.Warrior{}).
+		Owns(&appsv1.Deployment{}).
+		Complete(r)
+}
+
+// deploymentForMemcached returns a Memcached Deployment object
+func (r *WarriorReconciler) deploymentForWarrior(
+	warrior *warriorv1alpha1.Warrior) (*appsv1.Deployment, error) {
+	ls := labelsForWarrior(warrior.Spec.Project)
+	replicas, err := replicasForWarrior(warrior)
+	if err != nil {
+		return nil, err
+	}
+	// Get the Operand image
+	image, err := imageForWarrior(warrior.Spec.Project)
+	if err != nil {
+		return nil, err
+	}
+
+	cacheSize, err := resource.ParseQuantity("512Mi")
+	if err != nil {
+		return nil, err
+	}
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      warrior.Name,
+			Namespace: warrior.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Volumes: []corev1.Volume{
+						{Name: "cache", VolumeSource: corev1.VolumeSource{
+							EmptyDir: &corev1.EmptyDirVolumeSource{
+								Medium:    "Memory",
+								SizeLimit: &cacheSize,
+							}},
+						},
+					},
+					// Warrior ONLY supports amd64 at the moment.
+					Affinity: &corev1.Affinity{
+						NodeAffinity: &corev1.NodeAffinity{
+							RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+								NodeSelectorTerms: []corev1.NodeSelectorTerm{
+									{
+										MatchExpressions: []corev1.NodeSelectorRequirement{
+											{
+												Key:      "kubernetes.io/arch",
+												Operator: "In",
+												Values:   []string{"amd64"},
+											},
+											{
+												Key:      "kubernetes.io/os",
+												Operator: "In",
+												Values:   []string{"linux"},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					SecurityContext: &corev1.PodSecurityContext{
+						RunAsNonRoot: &[]bool{true}[0],
+						SeccompProfile: &corev1.SeccompProfile{
+							Type: corev1.SeccompProfileTypeRuntimeDefault,
+						},
+					},
+					Containers: []corev1.Container{{
+						Image:           image,
+						Name:            "warrior",
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						Env: []corev1.EnvVar{
+							{Name: "CONCURRENT_ITEMS", Value: strconv.Itoa(warrior.Spec.Scaling.Concurrency)},
+							{Name: "DOWNLOADER", Value: warrior.Spec.Downloader},
+						},
+						VolumeMounts: []corev1.VolumeMount{
+							{MountPath: "/grab/data", Name: "cache"},
+						},
+						SecurityContext: &corev1.SecurityContext{
+							RunAsNonRoot:             &[]bool{true}[0],
+							RunAsUser:                &[]int64{1001}[0],
+							AllowPrivilegeEscalation: &[]bool{false}[0],
+							Capabilities: &corev1.Capabilities{
+								Drop: []corev1.Capability{
+									"ALL",
+								},
+							},
+						},
+						Args: []string{"--concurrent", "$(CONCURRENT_ITEMS)", "$(DOWNLOADER)"},
+					}},
+				},
+			},
+		},
+	}
+
+	if err := ctrl.SetControllerReference(warrior, dep, r.Scheme); err != nil {
+		return nil, err
+	}
+	return dep, nil
+}
+
+type stats struct {
+	Counts struct {
+		Done int     `json:"done"`
+		Todo int     `json:"todo"`
+		Rcr  float64 `json:"rcr"`
+		Out  int     `json:"out"`
+	} `json:"counts"`
+}
+
+func replicasForWarrior(warrior *warriorv1alpha1.Warrior) (int32, error) {
+	url := fmt.Sprintf("https://legacy-api.arpa.li/%s/stats.json", warrior.Spec.Project)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making HTTP request:", err)
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	var s stats
+	err = json.Unmarshal(body, &s)
+	if err != nil {
+		return 0, err
+	}
+	if s.Counts.Todo > 1_000_000 {
+		return int32(warrior.Spec.Scaling.Maximum), nil
+	}
+	if s.Counts.Todo == 0 && s.Counts.Out > 1_000_000 {
+		return int32((warrior.Spec.Scaling.Minimum + warrior.Spec.Scaling.Maximum) / 2), nil
+	}
+	return int32(warrior.Spec.Scaling.Minimum), nil
+}
+
+// labelsForMemcached returns the labels for selecting the resources
+// More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/
+func labelsForWarrior(project string) map[string]string {
+	var imageTag string
+	image, err := imageForWarrior(project)
+	if err == nil {
+		imageTag = strings.Split(image, ":")[1]
+	}
+	return map[string]string{"app.kubernetes.io/name": "warrior-operator",
+		"app.kubernetes.io/version":    imageTag,
+		"app.kubernetes.io/managed-by": "WarriorController",
+	}
+}
+
+// imageForMemcached gets the Operand image which is managed by this controller
+// from the MEMCACHED_IMAGE environment variable defined in the config/manager/manager.yaml
+func imageForWarrior(project string) (string, error) {
+	var imageEnvVar = "WARRIOR_IMAGE_BASE"
+	image, found := os.LookupEnv(imageEnvVar)
+	if !found {
+		return "", fmt.Errorf("unable to find %s environment variable with the image", imageEnvVar)
+	}
+	return fmt.Sprintf("%s/%s-grab:latest", image, project), nil
+}
